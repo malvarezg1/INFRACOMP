ecde4a62069cebf52b90b0354d97635b2867b075
Nicolas Bejar <n.bejar@uniandes.edu.co>
2021-04-25 15:52:26 -0500

    Terminado

---
+++ Caso 3/src/uniandes/Prototipo.java
@@ -0,0 +1,68 @@
+package uniandes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Prototipo {
+
+	public static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	public static final int PASSWORD_MAX_LENGTH = 7;
+	public static long START_TIME;
+
+	public static int numCeros ;
+	public static String CADENA_INICIAL;
+	public static String algoritmo;
+
+
+	public static byte[] hexStringToByteArray(String s) {
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) {
+			data[i / 2] = (byte)((Character.digit(s.charAt(i), 16) << 4) +
+					Character.digit(s.charAt(i + 1), 16));
+		}
+		return data;
+	}
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Porfavor ingrese el algoritmo (SHA-256 y SHA-512)");
+		algoritmo = in.nextLine();
+        System.out.println("Porfavor ingrese la cadena de caracteres inicial ( no superior a 32 caracteres)");
+		CADENA_INICIAL = in.nextLine();
+		System.out.println("Porfavor ingrese el numero de ceros buscados (20, 24, 28, 32 o 36 )");
+		numCeros = Integer.parseInt(in.nextLine())/4;
+
+		int cores = Runtime.getRuntime().availableProcessors();
+		int lengthSet = PASSWORD_MAX_LENGTH / cores; // Cuantos dígitos le asigno a cada core
+		int end = 0;
+
+		START_TIME = System.currentTimeMillis();
+
+		ExecutorService executorService = Executors.newFixedThreadPool(cores);
+
+		while (end < PASSWORD_MAX_LENGTH) {
+			int start = end + 1;
+			end = start + lengthSet;
+
+			if (end > PASSWORD_MAX_LENGTH)
+				end = PASSWORD_MAX_LENGTH;
+
+			executorService.submit(new Validador(start, end, algoritmo));
+		}
+
+		executorService.shutdown();
+
+	}
+
+}
+
--- Caso 3/src/uniandes/Validador.java
+++ Caso 3/src/uniandes/Validador.java
@@ -5,7 +5,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-class Cracker implements Runnable {
+class Validador implements Runnable {
 
 	private int start;
 	private int end;
@@ -13,18 +13,16 @@ class Cracker implements Runnable {
 	private static boolean DONE = false;
 	private String found;
 
-	public Cracker(int s, int e, String algoritmo) throws NoSuchAlgorithmException {
+	public Validador(int s, int e, String algoritmo) throws NoSuchAlgorithmException {
 		start = s;
 		end = e;
 		digest = MessageDigest.getInstance(algoritmo);
 	}
 
 	public String[] concatenar(String candidato) {
-
 		String[] rta = new String[2];
-		rta[0] =  candidato + PasswordCracker.CADENA_INICIAL;
-		rta[1] = PasswordCracker.CADENA_INICIAL + candidato;
-
+		rta[0] =  candidato + Prototipo.CADENA_INICIAL;
+		rta[1] = Prototipo.CADENA_INICIAL + candidato;
 		return rta;
 	}
 
@@ -44,14 +42,14 @@ class Cracker implements Runnable {
 		    StringBuilder a1 = new StringBuilder();
 		    StringBuilder a2 = new StringBuilder();
 		    
-		    for (int i = 0; i < PasswordCracker.numCeros/2; i++) {
+		    for (int i = 0; i < Prototipo.numCeros/2; i++) {
 		        a1.append(String.format("%02X", bytes1[i]));
 		        a2.append(String.format("%02X", bytes2[i]));
 			}
 		    			
 			boolean funciono1 = true;
 			boolean funciono2 = true;
-			for (int i = 0; i < PasswordCracker.numCeros; i++) {
+			for (int i = 0; i < Prototipo.numCeros; i++) {
 				if(a1.charAt(i) != '0') {
 					funciono1 = false;
 				}
@@ -71,12 +69,11 @@ class Cracker implements Runnable {
 			return;
 		}
 
-		for (int i = 0; i < PasswordCracker.ALPHABET.length && !DONE; i++) {
-			char letter = PasswordCracker.ALPHABET[i];
+		for (int i = 0; i < Prototipo.ALPHABET.length && !DONE; i++) {
+			char letter = Prototipo.ALPHABET[i];
 			sb.setCharAt(n, letter);
 			generate(sb, n + 1);
 		}
-
 	}
 
 	@Override
@@ -88,7 +85,7 @@ class Cracker implements Runnable {
 		}
 
 		if (DONE) {
-			long duration = System.currentTimeMillis() - PasswordCracker.START_TIME;
+			long duration = System.currentTimeMillis() - Prototipo.START_TIME;
 			System.out.println("Cadena encontrada en  " + TimeUnit.MILLISECONDS.toSeconds(duration) + "." + TimeUnit.MILLISECONDS.toMillis(duration) + " segundos. La cadena fue = " + found);
 		} else {
 			System.out.println("Cadena no encontrada para set de [" + start + ", " + end + "] caracteres");
